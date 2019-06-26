d3e1319be558ae0b17ab11b703a3bc36759d9a1f
Ramon Saraiva <ramonsaraiva@gmail.com>
2019-06-25 21:51:24 -0300

    Add lyric path to main module

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     with open('dataset/songs.csv', 'r') as dataset:
         dataset_reader = csv.reader(dataset, delimiter=',')
         for song in dataset_reader:
-            song, author, feat = song
-            print(song, author, feat)
+            song, author, feat, lyric_path = song
+            print(song, author, feat, lyric_path)
 
 
 if __name__ == '__main__':
